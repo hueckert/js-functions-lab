eacdd2a115f1e01ca9cf3af5ea8c22ff59894f84
howard <howard.ueckert@protonmail.com>
2024-10-14 21:08:11 -0500

    ex 10

--- app.js
+++ app.js
@@ -197,4 +197,39 @@ function basicCalculator (num1, num2, oper){
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
 
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade (grade){
+    if (grade >=90){
+        return "A"
+    }else if
+        (grade >= 80) 
+        return "B"
+    else if 
+        (grade >= 70)
+        return "C" 
+    else if 
+        (grade >= 60)
+        return "D" 
+    else
+        (grade < 60)
+        return "F" 
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+
 
